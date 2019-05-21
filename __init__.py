dd021b16f96c267bcd5f077e7c2ea3430ded842e
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-05-20 20:52:21 -0400

    init file change

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 from comparative_1.guess import guess
 from comparative_1.stringDatabase import stringDatabase
-
+from comparative_1.game import game
 
 def main():
     print("** The great guessing game **")
 
     stringDatabaseObject = stringDatabase()
     guessObject =guess()
+    gameObject = game()
     stringDatabaseObject.readfile()
     guessObject.calling()
+    gameObject.finalPrint()
 
 main()
