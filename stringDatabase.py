d1469759b95a2f9436c11fa7ba6a3c6d7cb0b128
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-05-21 17:56:02 -0400

    string Database changes

--- stringDatabase.py
+++ stringDatabase.py
@@ -1,7 +1,20 @@
 import random
+
+from game import game
+
+
 class stringDatabase:
+    gameObj = game()
+    gameEnd = ()
     w_lis = []
     def readfile(self):
+
+        """
+        This function reads the file four_letters.txt from the storage and seperates them by converting it to list of elements
+        and finally retrieving random values from the list and returning the value to the
+        :return: random word as a string
+        """
+
         with open('four_letters.txt') as f:
             for x in f:
                 self.w_lis.extend(x.split())
@@ -11,3 +24,4 @@ class stringDatabase:
         self.current_word = random.choice(self.w_lis)
         #print(self.current_word)
         return  self.current_word
+
