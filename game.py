d56fbd21c1250462cb96361bb333105a68f95129
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2019-05-21 17:53:09 -0400

    game class changes

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ import sys
 class game(object):
     outputList = []
     outputList2 = []
+    outputList3 = []
+    finalValue = ()
     #def __init__(self, game, word, status, guess, letter, score):
     #    self.game = game;
     #    self.word = word;
@@ -14,13 +16,15 @@ class game(object):
     #    self.score = score;
     #    print()
 
+
+
     def __init__(self):
         print()
 
     def finalPrint(self):
-        from comparative_1 import guess
+        from guess import guess
         go = guess()
-        self.outputList = go.calling()
+        self.outputList = go.final_list
         #self.outputList2 = list(filter(None.__ne__, self.outputList))
         #print(self.outputList2)
         for i in self.outputList:
@@ -29,16 +33,16 @@ class game(object):
             else:
                 self.outputList2.append(i)
 
-        #print(self.outputList2)
+        self.outputList3 = list(filter(None.__ne__, self.outputList2))
+        self.finalvalue = tuple(self.outputList3)
+
+        #print(self.outputList3)
 
         print("game    word    status    Bad Guesses    Missed Letters    score")
         print("----    ----    ------    -----------    --------------    -----")
         i = 0
         j = 0
-        while i < len(self.outputList2):
+        while i < len(self.finalvalue):
 
-            print(self.outputList2[i][0], '     ', self.outputList2[i][1], '  ', self.outputList2[i][2], ' ', self.outputList2[i][3], '            ', self.outputList2[i][4],'               ', self.outputList2[i][5])
+            print(self.finalvalue[i][0], '     ', self.finalvalue[i][1], '  ', self.finalvalue[i][2], ' ', self.finalvalue[i][3], '            ', self.finalvalue[i][4],'               ', self.finalvalue[i][5])
             i = i+1
-
-
-
